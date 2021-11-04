c78ed24f2c2d189d5720a741504b8aaf7e8f99f9
noseong-ung <asdz453@gmail.com>
2021-11-04 20:34:53 +0900

    Initial commit

---
+++ src/main/java/com/board/practice_board/PracticeBoardApplication.java
@@ -0,0 +1,13 @@
+package com.board.practice_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/board/practice_board/PracticeBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.board.practice_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
